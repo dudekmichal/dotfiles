6aecf8eae963e87925342a8de6d9f7c22589d364
Michal Dudek <michal.dudek1995@gmail.com>
2018-06-12 08:58:27 +0200

    Updated: config files

--- .i3/rofi.sh
+++ .i3/rofi.sh
@@ -1,8 +1,7 @@
 rofi -show run -modi run -location 0 -width 50 \
-         -lines 5 -line-margin 0 -line-padding 1 \
-         -separator-style none -font "Inconsolata 10" -columns 5 -bw 0 \
-         -color-window "#0e0e0e, #0e0e0e, #b1b4b3" \
-         -color-normal "#0e0e0e, #b1b4b3, #0b0b0b, #ff0000, #000000" \
-         -color-active "#000000, #000000, #0e0e0e, #ff0000, #000000" \
-         -color-urgent "#000000, #000000, #0e0e0e, #ff0000, #000000" \
-         -kb-row-select "Tab" -kb-row-tab ""
+    -lines 5 -line-margin 0 -line-padding 1 \ -separator-style none -font "Terminus 9" -columns 5 -bw 0 \
+    -color-window "#0e0e0e, #0e0e0e, #b1b4b3" \
+    -color-normal "#0e0e0e, #b1b4b3, #0b0b0b, #ff0000, #000000" \
+    -color-active "#000000, #000000, #0e0e0e, #ff0000, #000000" \
+    -color-urgent "#000000, #000000, #0e0e0e, #ff0000, #000000" \
+    -kb-row-select "Tab" -kb-row-tab ""
